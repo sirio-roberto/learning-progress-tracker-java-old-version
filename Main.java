aa8525f57c20029de7ebe0e1acc97f40160bc419
Sirio Junior <sirioroberto1@hotmail.com>
2023-03-19 09:57:23 -0300

    Initial project with user manu created

---
+++ Main.java
@@ -0,0 +1,34 @@
+package tracker;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        startProgram();
+    }
+
+    private static void startProgram() {
+        System.out.println("Learning Progress Tracker");
+        showMainMenu();
+    }
+
+    private static void showMainMenu() {
+        String userInput;
+        do {
+            userInput = scan.nextLine();
+            switch (userInput) {
+                case "exit" -> System.out.println("Bye!");
+                default -> validateNonExpectedInput(userInput);
+            }
+        } while (!"exit".equals(userInput));
+    }
+
+    private static void validateNonExpectedInput(String userInput) {
+        if (userInput.matches("\\s*")) {
+            System.out.println("No input.");
+        } else {
+            System.out.println("Error: unknown command!");
+        }
+    }
+}
